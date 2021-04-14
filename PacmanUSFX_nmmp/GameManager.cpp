5fa5812a6a2aaf8a634ea6994efcdc28e943b703
nmmartinezp <nikimartin56@gmail.com>
2021-04-14 18:38:18 -0400

    C4

--- PacmanUSFX_nmmp/GameManager.cpp
+++ PacmanUSFX_nmmp/GameManager.cpp
@@ -1,1 +1,52 @@
 #include "GameManager.h"
+
+#include "SDL.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "SDL could not initialize! SDL_Error: %s\n", SDL_GetError();
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("GAME Pacman_nmmp", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 40, 40, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            cout<<"Window could not be created! SDL_Error: %s\n", SDL_GetError();
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Wait two seconds
+            SDL_Delay(10000);
+        }
+    }
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
